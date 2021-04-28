c6868ca1882c105515a6d8c9b44633dd5e86240c
Hgwxxdd <598777720@qq.com>
2021-04-28 02:51:16 +0000

    Deploying to gh-pages from @ Hgwxxdd/github-actions-weather-bot@08017071db14b6509a4e19a9bc338c8c53049bd3 🚀

---
+++ README.md
@@ -0,0 +1,5 @@
+# github-actions-weather-bot
+
+使用 `Github Actions` 实现定时发送穿衣建议邮件。
+
+实现过程放在了[这里](https://juejin.cn/post/6950551485356703751)。
---
+++ index.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function init()  {
+  let wear, wearMsg, tem, temData, temMsg, date = new Date();
+
+  // 获取穿衣建议
+  // 后面的数字跟城市相关，可以在中国天气网访问对应城市看到
+  wear = await axios.get('http://www.weather.com.cn/weather1d/101270101.shtml')
+  $ = cheerio.load(wear.data)
+  wearMsg = `穿衣建议：${$("#chuanyi p").text()}`
+
+  // 获取温度
+  // 这个接口用于获取当前温度信息，后面的 query 参数是应该是当前时间的时间戳
+  tem = await axios.get(`http://d1.weather.com.cn/sk_2d/101270101.html?_=${date.getTime()}`)
+  eval(tem.data)
+  temMsg = `当前温度：${dataSK.temp} ℃`
+
+  // 日期
+  dayMsg = `今天是${date.getFullYear()}年${date.getMonth() + 1 }月${date.getDate()}日`
+
+  // 返回的 html
+  let context = `
+  <html>
+  　<head>
+  　　<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  　</head>
+    <body>
+      <p>${dayMsg}</p>
+      <p>${temMsg}</p>
+      <p>${wearMsg}</p>
+    </body>
+  </html>
+`
+
+  fs.writeFileSync('index.html', context, 'utf8')
+}
+
+init()
+
